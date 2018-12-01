ab3d410c64127611034979dd17feabfb72d12e44
asterixorobelix <nathanstasin@gmail.com>
2018-12-01 18:10:57 +0200

    add image assets. flip which button is enabled

--- pitchPerfect/PitchPerfect/PitchPerfect/ViewController.swift
+++ pitchPerfect/PitchPerfect/PitchPerfect/ViewController.swift
@@ -12,18 +12,30 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
     
+    @IBOutlet weak var recordingButton: UIButton!
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        stopRecordingButton.isEnabled = false;
     }
 
     @IBAction func recordAudio(_ sender: UIButton) {
         print("record button pressed")
         recordingLabel.text = "recording in progress"
+        stopRecordingButton.isEnabled = true;
+        recordingButton.isEnabled = false;
     }
     
     @IBAction func stopRecording(_ sender: UIButton) {
-        print("stop recording button pressed")
+        recordingLabel.text = "tap to record";
+        stopRecordingButton.isEnabled = false;
+        recordingButton.isEnabled = true;
     }
 }
 
